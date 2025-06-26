7c7c7af7236e17bee26dd9856a9899843d17d465
vkyl <viktor.kyliukh@gmail.com>
2025-06-26 23:44:36 +0300

    fix styles

--- src/utils/components/Header.tsx
+++ src/utils/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <AppBar position="static" color="secondary" elevation={2}>
       <Toolbar>
-        <Box flexGrow={1} sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', gap: "15px"}}>
+        <Box flexGrow={1} sx={{display: 'flex', alignItems: 'center', justifyContent: 'flex-start', gap: "15px", paddingY: "10px"}}>
           <img src={Logo} alt="logo" width={55} height={55} />
           <Typography variant="h6" component="div">
             InvestFolio
